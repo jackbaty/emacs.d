54a4ae18cda72674055bc8a3c6a0f473434fe731
Jack Baty <jack@baty.net>
2021-03-02 06:03:21 -0500

    Move bookmark file back to default location

--- init.el
+++ init.el
@@ -175,7 +175,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key evil magit org-journal counsel)))
+ '(package-selected-packages '(markdown-mode which-key evil magit org-journal counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
